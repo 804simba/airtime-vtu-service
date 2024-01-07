ba05efe089e4099dc12b7a4b21f435d5eb31f89f
Timothy Ngonadi <timothyolisaeloka@gmail.com>
2024-01-07 19:17:38 +0100

    remove open feign

--- src/main/java/com/xpresspayments/api/ApiApplication.java
+++ src/main/java/com/xpresspayments/api/ApiApplication.java
@@ -2,10 +2,8 @@ package com.xpresspayments.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
